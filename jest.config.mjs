1d1f76dc25c0598e792e5203a44c97cd6131a542
Sarah Jehin <sarah@calibrate.be>
2025-04-29 14:47:30 +0200

    [test] Test reports

--- jest.config.mjs
+++ jest.config.mjs
@@ -107,7 +107,11 @@ const config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    ['jest-junit', {outputDirectory: 'reports', outputName: 'report.xml'}],
+    ['github-actions', {silent: false}],
+    'summary'
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
