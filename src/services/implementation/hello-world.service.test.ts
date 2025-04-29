9b2fbe2adee93e758d110b4d4d4820d6a0fd1571
Jonas Claes <jonas.claes@qaura.be>
2025-04-29 13:41:34 +0200

    chore: add jest example

---
+++ src/services/implementation/hello-world.service.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "@jest/globals";
+import { HelloWorldServiceImpl } from "./hello-world.service.ts";
+
+describe("sum module", () => {
+  test("check true", async () => {
+    const helloWorldService = new HelloWorldServiceImpl();
+
+    const result = await helloWorldService.helloWorld();
+
+    expect(result).toBe("Hello, World!");
+  });
+});
